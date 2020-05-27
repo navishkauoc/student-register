4b9214871793c1cf879535e054aff15ad5447214
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-27 21:22:52 +0530

    Student entity class created

---
+++ Backend/StudentRegister/src/main/java/com/navishka/studentregister/entities/Student.java
@@ -0,0 +1,69 @@
+package com.navishka.studentregister.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private double age;
+	private String gender;
+	private String photo;
+	
+	public Student() {
+	}
+	
+	public Student(String name, double age, String gender, String photo) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.photo = photo;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getAge() {
+		return age;
+	}
+
+	public void setAge(double age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+}
