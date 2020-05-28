21e57c360b992d6e04f0e1e74c385d27df9d299d
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-28 17:38:40 +0530

    Paths added to app-routing.module.ts

--- Frontend/studreg/src/app/app-routing.module.ts
+++ Frontend/studreg/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'student', component: StudentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
