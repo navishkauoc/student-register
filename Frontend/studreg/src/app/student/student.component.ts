60b675812aa3d44ea03ebc9b71ebfe7e0ba0e43e
Navishka Kularathna <navishka.randika@gmail.com>
2020-05-29 11:31:52 +0530

    Data receiving from client-side

--- Frontend/studreg/src/app/student/student.component.ts
+++ Frontend/studreg/src/app/student/student.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//01. Create an interface for Single object
+export interface Student{
+  id: string;
+  name: string;
+  age: string;
+  gender: string;
+  photo: string;
+}
 
 @Component({
   selector: 'app-student',
@@ -7,9 +17,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentComponent implements OnInit {
 
-  constructor() { }
+  //02. Create array variable for all objects
+  students: Student[]= [];
+
+  //03. Http Object injection
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    //04. Load data from URL
+    var url= "http://localhost:8080/customers";
+    this.http.get<Student[]>(url)
+    .subscribe(data=>{
+      this.students= data;
+    })
   }
+
+  
 
 }
