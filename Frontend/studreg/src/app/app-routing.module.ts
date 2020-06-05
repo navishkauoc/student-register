147e06978a74edf4df9b8308e51901c9730009b6
Navishka Kularathna <navishka.randika@gmail.com>
2020-06-05 22:55:27 +0530

    Subject component connected

--- Frontend/studreg/src/app/app-routing.module.ts
+++ Frontend/studreg/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StudentComponent } from './student/student.component';
 import { StudentContactComponent } from './student-contact/student-contact.component';
+import { SubjectComponent } from './subject/subject.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'student', component: StudentComponent},
-  {path: 'student-contact', component: StudentContactComponent}
+  {path: 'student-contact', component: StudentContactComponent},
+  {path: 'subject', component: SubjectComponent}
 ];
 
 @NgModule({
